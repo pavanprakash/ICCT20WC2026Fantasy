e12bca7fda41f312726469bf110007770f231480
pavanprakash <pavanprakash@gmail.com>
2026-03-05 10:43:27 +0000

    fixture changes

--- client/src/data/fixtures-2026.js
+++ client/src/data/fixtures-2026.js
@@ -67,8 +67,8 @@ const fixtures = [
   { date: "2026-03-01", time: "6:00 PM", timeGMT: "12:30", team1: "IND", team2: "WI", venue: "Kolkata", stage: "Super 8" },
 
   { date: "2026-03-04", time: "1:30 PM", timeGMT: "13:30", team1: "SA", team2: "NZ", venue: "Eden Gardens, Kolkata", stage: "Semi-Final" },
-  { date: "2026-03-05", time: "1:30 PM", timeGMT: "13:30", team1: "IND", team2: "ENG", venue: "Wankhede Stadium, Mumbai", stage: "Semi-Final" },
-  { date: "2026-03-08", time: "1:30 PM", timeGMT: "13:30", team1: "TBC", team2: "TBC", venue: "Narendra Modi Stadium, Ahmedabad", stage: "Final" }
+  { id: "937c08ca-c439-4ad0-9f17-6c6f6875076e", date: "2026-03-05", time: "1:30 PM", timeGMT: "13:30", team1: "IND", team2: "ENG", venue: "Wankhede Stadium, Mumbai", stage: "Semi-Final" },
+  { id: "b8833e89-6997-4075-ad6a-426b9968e2ef", date: "2026-03-08", time: "1:30 PM", timeGMT: "13:30", team1: "NZ", team2: "TBC", venue: "Narendra Modi Stadium, Ahmedabad", stage: "Final" }
 ];
 
 export default fixtures;
